45fc98a857298eb82842d954a9a30b3e02f7ba4b
ThePrimeagen <samueljamieson02@gmail.com>
2024-11-20 18:37:29 +0000

    user can guess letters as well as the word itself

--- hangman.py
+++ hangman.py
@@ -9,8 +9,35 @@ hard = "hard.txt"
 
 #get user input
 def main():
-    word = randomword(easy)
+    word = randomword(easy).lower()
     current = getblankword(word)
+    lives = 8
+    usedletters = set()
+    print(" ".join(current))
+    print(word)
+    while True:
+        guess = input("Guess word Or Letter: ")
+
+        if len(guess) > 1:
+            if guess == word:
+                print("win")
+                break
+            else:
+                lives -= 1
+                print(f"Incorrect you have {lives} left")
+        else:
+            if guess in word:
+                num = 0
+                for x in word:
+                    
+                    if x == guess:
+                        current[num] = guess
+                    num +=1
+        print(" ".join(current))
+        if "".join(current) == word:
+            print("win")
+            break
+       
 
 
 
