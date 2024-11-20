42bf89c3eb81686109b1cee236a5559d11e55af5
ThePrimeagen <samueljamieson02@gmail.com>
2024-11-20 18:19:10 +0000

    made the difficulties and made it so it choses a random word out of the lot

--- hangman.py
+++ hangman.py
@@ -1,1 +1,34 @@
-print("hello world")
+import random
+easy = "easy.txt"
+med = "med.txt"
+hard = "hard.txt"
+
+
+
+
+
+#get user input
+def main():
+    word = randomword(hard)
+    print(word)
+
+
+
+
+def randomword(dificulty):
+    words = []
+    f = open(dificulty, "r")
+    for x in f:
+        words.append(x.strip())
+    x = random.randrange(0,len(words))
+    return words[x]
+
+    
+
+
+
+
+
+
+
+main()
